10c91acab143408f78d39dc36f6a126936cc5247
unknown <sameerdutta9703@gmail.com>
2020-04-21 10:46:35 +0530

    changes

--- Task2/myapp/routes/index.js
+++
@@ -1,9 +0,0 @@
-var express = require('express');
-var router = express.Router();
-
-/* GET home page. */
-router.get('/api', function (req, res, next) {
-  res.status(200).json({ text: 'This is the Body section' });
-});
-
-module.exports = router;
